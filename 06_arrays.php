48291def0b5808506325a8fc544e4d547cd6e4c8
Sanderb-immalle <sander.backx@immalle.be>
2017-02-20 09:33:19 +0100

    oefening7

--- 06_arrays.php
+++ 06_arrays.php
@@ -2,16 +2,35 @@
 
 // Maak een Array met 3 namen van medeleerlingen.
 
+$names = array("Remco", "Axel", "Kenzi");
 
 // Itereer er over met een for-loop. Print ze af in een HTML ongenummerde lijst.
 
+for ($i = 0; $i <= count($names); $i++){
+
+echo $names[$i];
+echo "<br>";
+
+}
 
 // Itereer er over met een foreach-loop. Print ze af in HTML genummerde lijst.
 
+foreach($names as $name){
 
+echo $name;
+echo "<br>";
+
+}
+echo "<br>";
 
 $naam = "Willy";
 
 // Itereer met een for-loop over de variable $naam alsof het een array was.
+for ($i = 0; $i <= count($naam); $i++){
+
+echo $naam[$i];
+echo "<br>";
+
+}
 
 ?>
