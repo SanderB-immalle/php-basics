082d9978716e8873145e0fb33cff9ab93c0457ba
Sanderb-immalle <sander.backx@immalle.be>
2017-02-20 09:16:18 +0100

    oefening4

--- 03_string_functions.php
+++ 03_string_functions.php
@@ -4,7 +4,9 @@ $zin = "Binnenkort is het weeral lente";
 
 // Print af hoeveel woorden $zin bevat.
 
+echo str_word_count($zin,0);
 
+echo str_replace("lente", "zomer", $zin );
 // Vervang in $zin "lente" door "zomer" en druk af.
 
 
