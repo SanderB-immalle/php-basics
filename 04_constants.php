fd311e8ca425686015f2d3728dc374fcf86dedd8
Sanderb-immalle <sander.backx@immalle.be>
2017-02-20 09:25:46 +0100

    oefening5

--- 04_constants.php
+++ 04_constants.php
@@ -1,8 +1,14 @@
 <?php
 
 define("USERNAME", "joske");
+define("PASSWORD", "imma");
+define("SERVERNAME", "php");
 
 echo USERNAME;
+echo "<br>";
+echo PASSWORD;
+echo "<br>";
+echo SERVERNAME;
 
 // definieer ook een constante PASSWORD en SERVERNAME
 
