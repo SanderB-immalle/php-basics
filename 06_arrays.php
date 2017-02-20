53cdad54cefe2b11d272d3f44a084ebdd78f0ee3
Sanderb-immalle <sander.backx@immalle.be>
2017-02-20 09:48:31 +0100

    oefening6fix

--- 06_arrays.php
+++ 06_arrays.php
@@ -5,22 +5,23 @@
 $names = array("Remco", "Axel", "Kenzi");
 
 // Itereer er over met een for-loop. Print ze af in een HTML ongenummerde lijst.
+echo "<ul>";
+for ($i = 0; $i < count($names); $i++){
 
-for ($i = 0; $i <= count($names); $i++){
-
-echo $names[$i];
-echo "<br>";
+echo "<li>$names[$i]</li>";
 
 }
+echo "</ul>";
 
 // Itereer er over met een foreach-loop. Print ze af in HTML genummerde lijst.
-
+echo "<ol>";
 foreach($names as $name){
 
-echo $name;
-echo "<br>";
+echo "<li>$name</li>";
+
 
 }
+echo "</ol>";
 echo "<br>";
 
 $naam = "Willy";
